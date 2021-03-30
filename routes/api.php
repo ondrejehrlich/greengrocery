f785101c92dddab8144bc49d0a7a19bcf684a082
ondrejehrlich <ondrej.ehrlich@gmail.com>
2021-03-30 17:24:03 +0200

    ADD: api routing created

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('/product', ProductController::class)->only([
+    'index', 'show', 'store', 'update', 'destroy',
+]);
+Route::resource('/boxes', BoxController::class)->only([
+    'index', 'show', 'store', 'destroy',
+]);
+
+Route::get('/boxes/{box}/product/{product}', [BoxController::class, 'addProductToBox']);
